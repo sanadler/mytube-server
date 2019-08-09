a4d739146b774a16a746f16a34bd4ac9474907fc
Sarah Nadler <sarah.m.nadler@gmail.com>
2019-08-08 19:40:30 -0600

    trying to fix delete

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use(
     })
 );
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 //const videosRouter = require('./videosRouter');
 app.use(require('./jwtRouter'));
 app.use(require('./videosRouter'));
